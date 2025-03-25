f7069d2de6cf2c734ebd0ee7fde882b6732403f8
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-25 10:21:21 +0000

    add home, blog, about, contact blade.php

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>halaman blog</title>
+</head>
+<body>
+<a href="/">home</a>
+<a href="/blog">blog</a>
+<a href="/about">about</a>
+<a href="/contact">contact</a>
+    <h1>halaman blog</h1>
+    <h3>owner :{{$nama}} </h3>
+</body>
+</html>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>home</title>
+    <title>halaman contact</title>
 </head>
 <body>
-    <a href="/">home</a>
-    <a href="/about">about</a>
-
-    <h1>halaman HOME</h1>   
+<a href="/">home</a>
+<a href="/about">contact</a>
+    <h1>halaman about</h1>
+    <h3>owner :{{$nama}} </h3>
 </body>
 </html>
