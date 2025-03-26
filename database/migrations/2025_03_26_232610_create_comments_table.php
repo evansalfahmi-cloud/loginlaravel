ae78e6595e7035dbf24be03a7968f3a03789d52c
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-26 23:26:28 +0000

    create migrate table for artikel_db

--- database/migrations/2025_03_26_232610_create_comments_table.php
+++ database/migrations/2025_03_26_232610_create_comments_table.php
@@ -4,21 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('article_id');
-            $table->string('name'); // Nama komentator
-            $table->text('comment'); // Isi komentar
             $table->timestamps();
-
-            // Foreign key ke tabel articles
-            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('comments');
