aa1c6024ff7fb9981d25599818a955851fe63c4b
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-26 10:24:40 +0000

    create posts

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,3 +1,33 @@
 <x-layout>
-    <p>Selamat datang di halaman blog.</p>
+    <div class="container mt-4">
+        <h1 class="text-center mb-4">Selamat Datang di Halaman Blog</h1>
+
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <article class="mb-4 p-4 border rounded shadow-sm bg-light">
+                    <h2 class="text-primary">Judul Artikel 1</h2>
+                    <div class="text-muted mb-2">
+                        <a href="#" class="text-decoration-none fw-bold">Evans Al Fahmi</a> | 1 Januari 2025
+                    </div>
+                    <p class="text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta reprehenderit quos totam suscipit eaque rem ea minus optio aspernatur expedita. Atque ipsum ab alias ea quaerat laborum repellat minima dolor.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero numquam natus voluptatum soluta beatae reprehenderit quo harum optio perspiciatis. In pariatur, aut placeat unde sapiente nisi sit corporis aliquid? At.
+                    </p>
+                    <a href="#" class="btn btn-primary">Read More &raquo;</a>
+                </article>
+
+                <article class="mb-4 p-4 border rounded shadow-sm bg-light">
+                    <h2 class="text-primary">Judul Artikel 2</h2>
+                    <div class="text-muted mb-2">
+                        <a href="#" class="text-decoration-none fw-bold">Evans Al Fahmi</a> | 2 Februari 2025
+                    </div>
+                    <p class="text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta reprehenderit quos totam suscipit eaque rem ea minus optio aspernatur expedita. Atque ipsum ab alias ea quaerat laborum repellat minima dolor.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero numquam natus voluptatum soluta beatae reprehenderit quo harum optio perspiciatis. In pariatur, aut placeat unde sapiente nisi sit corporis aliquid? At.
+                    </p>
+                    <a href="#" class="btn btn-primary">Read More &raquo;</a>
+                </article>
+            </div>
+        </div>
+    </div>
 </x-layout>
