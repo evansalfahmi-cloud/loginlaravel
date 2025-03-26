b43cc3eb8c3f76028e68300ba6c177948e7a0831
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-26 23:30:33 +0000

    edit  migrate file table for artikel_db

--- database/migrations/2025_03_26_232610_create_article_tag_table.php
+++ database/migrations/2025_03_26_232610_create_article_tag_table.php
@@ -1,27 +1,23 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('article_tag', function (Blueprint $table) {
+return new class extends Migration {
+    public function up() {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->string('title');
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->integer('views')->default(0);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('article_tag');
+    public function down() {
+        Schema::dropIfExists('articles');
     }
 };
