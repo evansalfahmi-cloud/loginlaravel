749bd2b58c4d3ff3fb6ef681663ca7b8830b4bb5
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-26 07:57:34 +0000

    add component layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>halaman blog</title>
+    <title>home</title>
     @vite(['resources/sass/app.scss','resources/js/app.js'])
 </head>
 <body>
+
 <x-navbar />
-<x-header title="Blog" />
+<x-header title="Home" />
 
 <div class="container mt-4">
-    <h1>Halaman Blog</h1>
+    {{$slot}}
 </div>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>home</title>
-    @vite(['resources/sass/app.scss','resources/js/app.js'])
-</head>
-<body>
-
-<x-navbar />
-
-<div class="container mt-4">
-    <h1>Welcome to My Website</h1>
-</div>
-</body>
-</html>
+<x-layout>
+  <h3>Ini halaman Home</h3></x-layout>
